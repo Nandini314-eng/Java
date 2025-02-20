ae95ee770c36370577f5a5c0389ee28d7fd5f674
Nandini314-eng <9045nandini@gmail.com>
2025-02-20 15:07:33 +0530

    Add files via upload

---
+++ pattern6.java
@@ -0,0 +1,29 @@
+public class pattern6 {
+  public static void main(String[] args) {
+    // nunber pyramid
+    int n=5;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            System.out.print("");
+        }
+        for(int j=1;j<=i;j++){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+    //palidrom no triangle
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            System.out.print("");
+        }
+        for(int j=i;j>=1;j--){
+            System.out.print(j);
+        }
+        for(int j=2;j<=i;j++){
+            System.out.print(j);
+        }
+        System.out.println();
+    }
+    
+  }  
+}
